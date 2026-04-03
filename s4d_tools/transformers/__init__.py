03ace824440377e10e74c4a32291ac2ae43ca2a2
Smaragda <smaragda.sarana@gmail.com>
2026-04-04 01:48:27 +0300

    Remove unused shape documentation imports from transformers __init__.py

--- s4d_tools/transformers/__init__.py
+++ s4d_tools/transformers/__init__.py
@@ -12,13 +12,9 @@ from .to_standardized import (
     transform_pin_to_standardized,
     transform_prd_to_standardized,
 )
-from .shape_documentation import get_shape_documentation, get_shape_structure
-
 __all__ = [
     "build_relative_price_longform",
     "expand_classic_apt_price_matrix",
-    "get_shape_documentation",
-    "get_shape_structure",
     "merge_apt_into_standardized",
     "price_matrix_from_any_apt_shape",
     "merge_pin_into_standardized",
